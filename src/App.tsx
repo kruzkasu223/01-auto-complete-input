1f9d29fd47f2d0473bacc7274c76625f1a22177e
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-02 18:39:12 +0530

    feat(styles): added necessary styles and elements

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,26 @@ function App() {
   return (
     <div className="App">
       <h1>01/27 - Auto Complete Input</h1>
+
+      <div className="form">
+        <div className="search_div" data-autocomplete="">
+          <input
+            className="search_input"
+            placeholder="Enter any fruit..."
+            type="text"
+            name="search"
+            id="search"
+            aria-autocomplete="both"
+          />
+          <div className="auto_complete_div">
+            {/* <button className="auto_complete_button">Hello1</button>
+            <button className="auto_complete_button">Hello2</button>
+            <button className="auto_complete_button">Hello3</button>
+            <button className="auto_complete_button">Hello4</button> */}
+          </div>
+        </div>
+        <input className="search_submit" type="submit" value="Submit" />
+      </div>
     </div>
   )
 }
