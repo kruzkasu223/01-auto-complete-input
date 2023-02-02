c938a46358b21ed89abf497d4e6d33975701db89
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-02 17:00:46 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>01/27 - Auto Complete Input</h1>
+    </div>
+  )
+}
+
+export default App
